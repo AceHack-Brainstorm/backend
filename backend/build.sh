4ac739752c6aa3b4a5e048cfed4afbd92f28f8ab
Piyush Dhall <contact@piyush.ovh>
2024-04-07 03:38:08 +0530

    changing dir structure

--- backend/build.sh
+++ backend/build.sh
@@ -5,8 +5,6 @@ set -o errexit
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
-cd backend
-
 # Convert static asset files
 python manage.py collectstatic --no-input
 
